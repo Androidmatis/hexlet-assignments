e28433481cb6abf45e37cc21cd7d4580dddf1400
@hexlet/cli <support@hexlet.io>
2024-02-11 22:06:03 +0600

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,37 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+class App {
+
+    public static Map<String, Integer> getWordCount(String sentence) {
+        var words = sentence.split(" ");
+        Map<String, Integer> wordsCount = new HashMap<>();
+        if (sentence.isEmpty()) {
+            return wordsCount;
+        }
+        for(String word : words) {
+            if (!wordsCount.containsKey(word)) {
+                wordsCount.put(word, 1);
+            } else {
+                wordsCount.put(word, wordsCount.get(word) + 1);
+            }
+        }
+        return wordsCount;
+    }
+
+    public static String toString(Map<String, Integer> wordsCount) {
+        if (wordsCount.isEmpty()) {
+            return "{}";
+        }
+        var result = "{\n";
+        for(Map.Entry<String, Integer> map : wordsCount.entrySet()) {
+            result +="  " + map.getKey() + ": " + map.getValue() + "\n";
+        }
+        result += "}";
+        return result;
+    }
+}
+//END
